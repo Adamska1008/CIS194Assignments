54aa8124a47bb63ecd8bac13d7bae1db919b526a
adamska <2639980868@qq.com>
2024-08-10 00:07:35 +0800

    finish homework 10

--- 10/AParser.hs
+++ 10/AParser.hs
@@ -81,3 +81,6 @@ intPair = (\x y -> [x, y]) <$> posInt <*> posInt
 instance Alternative Parser where
   empty = Parser (\input -> Nothing)
   (Parser a1) <|> (Parser a2) = Parser (\input -> a1 input <|> a2 input)
+
+intOUppercase :: Parser ()
+intOUppercase = ((\x -> ()) <$> posInt) <|> ((\x -> ()) <$> satisfy isUpper)
